6bfa4f843618c7a17367e0491a7aaca4113c41a7
qubitmatrix <preethi13m@gmail.com>
2024-12-11 16:23:56 +0530

    Optimized using parallelization

--- 2024/day11_1.py
+++ 2024/day11_1.py
@@ -1,24 +1,29 @@
 from timeit import default_timer as timer
+import multiprocessing
+def worker1(subarr):
+    res = []
+    for val in subarr:
+        if(val == 0):
+            ans = [1]
+        elif(len(str(val))%2 == 0):
+            split_1 = int(str(val)[:len(str(val))//2])
+            split_2 = int(str(val)[len(str(val))//2:])
+            ans = [split_1,split_2]
+        else:
+            ans = [val*2024]
+        res.extend(ans)
+    return res
+
 start = timer()
 inp = list(map(int, input().split()))
 count = 0
 while(count < 25):
-    pos = 0
-    while(pos < len(inp)):
-        val = inp[pos]
-        if(val == 0):
-            inp[pos] = 1
-        elif(len(str(val))%2 == 0):
-            split_1 = int(str(val)[:len(str(val))//2])
-            split_2 = int(str(val)[len(str(val))//2:])
-            inp[pos] = split_1
-            inp.append(-1)
-            inp[pos+2:] = inp[pos+1:-1]
-            inp[pos+1] = split_2
-            pos += 1
-        else:
-            inp[pos] *= 2024
-        pos += 1
+    num_workers = multiprocessing.cpu_count()
+    subarr_size = len(inp) // num_workers
+    subarrs = [inp[start:start+subarr_size] for start in range(0,len(inp),subarr_size)]
+    with multiprocessing.Pool(num_workers) as pool:
+        results = pool.map(worker1, subarrs)
+    inp = [val for result in results for val in result] 
     count += 1
     print(count)
 end = timer()
